db857e155f80a9c9d8e148475d2bac34c58dccfe
juligarji <jgarzonj@unal.edu.co>
2017-06-07 10:04:25 -0500

    change database variables

--- models/db/connection.js
+++ models/db/connection.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
 
+
 //mongoose.connect('mongodb://menj_test:menj_test@ds157641.mlab.com:57641/menjar_test');
 //mongoose.connect('mongodb://test_menjar:test_menjar@ds157571.mlab.com:57571/heroku_qgzwnns0');
+//mongoose.connect('mongodb://localhost/test');
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASS +'@ds111922.mlab.com:11922/heroku_cm4twdr9');
+mongodb://<dbuser>:<dbpassword>@ds111922.mlab.com:11922/heroku_cm4twdr9
 var db = mongoose.connection;
 
 module.exports = db;
