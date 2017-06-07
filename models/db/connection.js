cbe02dd44895f78e25d8d580ce4e76c644957f41
juligarji <jgarzonj@unal.edu.co>
2017-06-07 13:36:15 -0500

    keep alive every 5 min

--- models/db/connection.js
+++ models/db/connection.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
-
 //mongoose.connect('mongodb://menj_test:menj_test@ds157641.mlab.com:57641/menjar_test');
 //mongoose.connect('mongodb://test_menjar:test_menjar@ds157571.mlab.com:57571/heroku_qgzwnns0');
 //mongoose.connect('mongodb://localhost/test');
