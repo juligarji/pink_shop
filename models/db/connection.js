d3bafa2d2ec60f4df2fdfdd37d12eb44bdc51d22
juligarji <jgarzonj@unal.edu.co>
2017-07-06 10:55:21 -0500

    copia de seguridad para cambio mayor

--- models/db/connection.js
+++ models/db/connection.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost/test_monica');
+mongoose.connect('mongodb://localhost/test_monica');
 
 
 //mongoose.connect('mongodb://pinkStoreAdmin:pinkStoreAdmin@ds111922.mlab.com:11');
 
 
-mongoose.connect('mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASS +'@ds111922.mlab.com:11922/heroku_cm4twdr9');
+//mongoose.connect('mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASS +'@ds111922.mlab.com:11922/heroku_cm4twdr9');
 
 var db = mongoose.connection;
 
